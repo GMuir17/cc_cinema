73bd29b203066cd2c7a1963a88e81f34bba721b1
Gary Muir <garymuir17@gmail.com>
2018-06-08 16:28:55 +0100

    buy_ticket method added to Customer class, maybe. Seems to work, still unsure, but basic extensions finished

--- models/customer.rb
+++ models/customer.rb
@@ -74,13 +74,16 @@ class Customer
     SqlRunner.run(sql, values)
   end
 
-  # def buy_ticket(film)
-  #   # decrease the funds of customer by the film's price, and create a new ticket
-  #   sql = "INSERT INTO tickets (customer_id, film_id)
-  #     VALUES ($1, $2)
-  #     RETURNING id;"
-  #   values = []
-  # end
+  def buy_ticket(film)
+    # decrease the funds of customer by the film's price, and create a new ticket
+    new_ticket = Ticket.new({
+      "customer_id" => @id,
+      "film_id" => film.id()
+      })
+    new_ticket.save()
+    ticket_price = film.find_price()
+    decrease_funds(ticket_price)
+  end
 
 # class methods
   def self.all()
