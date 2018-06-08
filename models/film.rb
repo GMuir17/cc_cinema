4eeba94b597495791cd85c95d0143b90088e7f42
Gary Muir <garymuir17@gmail.com>
2018-06-08 11:47:58 +0100

    save methods written for Film and Ticket classes, added to console.rb

--- models/film.rb
+++ models/film.rb
@@ -11,7 +11,14 @@ class Film
     @price = options["price"].to_i()
   end
 
-  def save
+  def save()
+    sql = "INSERT INTO films (title, price)
+      VALUES ($1, $2)
+      RETURNING id;"
+    values = [@title, @price]
+    film = SqlRunner.run(sql, values).first()
+    @id = film["id"].to_i()
+  end
 
 
 end
