ef30c79e77354580cf8b5f0446795ead85d66e22
Gary Muir <garymuir17@gmail.com>
2018-06-08 11:50:59 +0100

    Customer.all() method added, tested

--- models/customer.rb
+++ models/customer.rb
@@ -5,6 +5,7 @@ class Customer
   attr_reader(:id)
   attr_accessor(:name, :funds)
 
+# instance methods
   def initialize(options)
     @id = options["id"].to_i() if options["id"]
     @name = options["name"]
@@ -20,5 +21,12 @@ class Customer
     @id = customer["id"].to_i()
   end
 
+# class methods
+  def self.all()
+    sql = "SELECT * FROM customers;"
+    customers = SqlRunner.run(sql)
+    results = customers.map {|customer| Customer.new(customer)}
+    return results
+  end
 
 end
